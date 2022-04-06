730f1270542c54fcd13456eed7cae778283ba410
Pablo Sousa <pablossilva.005@gmail.com>
2022-04-06 16:35:17 -0300

    Add files via upload

---
+++ ativ1/src/matriz.h
@@ -0,0 +1,15 @@
+#ifndef MATRIZ_H 
+#define MATRIZ_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+#define TAM 4
+
+void tamanhoMatriz();
+void gerarMatriz();
+void percorrerMatriz();
+
+#endif
